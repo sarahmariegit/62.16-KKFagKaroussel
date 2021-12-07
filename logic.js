35d4b3f9c2fcb407526a133622afa3d334a35389
lukj <lukj@itu.dk>
2021-12-07 08:56:01 +0100

    Finished small stuff

--- logic.js
+++ logic.js
@@ -1,37 +1,43 @@
-var ifSubjects = ["funktioner", "animationer"]
-var mathSubjects = ["differentialligninger", "vektorer i rummet"]
+var ifSubjects = ["funktioner", "animationer","eksempel emne1"] //nr 2
+var mathSubjects = ["differentialligninger", "vektorer i rummet", "eksempel emne1"] //nr 2
+//nr: 3 fysik, biotek, 
+//nr: 4 samf, engelsk 
+
 //Add new subject list
 var discs = ["af","bf"];
 var subs = ["a", "b"];
 $("#answer").val("ans");
 
+//nr 3 og 4
 var allSubjects = {
     //Add discipline
     "Informatik": ifSubjects,
     "Matematik": mathSubjects,
+
 }
 
-/**
- * Returns a list of two random subjects from the two given disciplines
- * @param {string[]} discs the two disciplines
- * @return {string[]} two random subjects
- */
-function randomSubjects(discs) {
 
-    return ["differentialligninger", "animationer"];
-}
 
-/**
+/**nr 5
  * Generate a random discipline by choosing in allSubject
  * @return {string[]} discs A list of two discipline, the first is always
  * informatik, the second is random. 
  */
 function randomDisciplines() {
-
+    //Math.random()
     return ["Informatik", "Matematik"];
 }
 
-/**
+/** nr 6
+ * Returns a list of two random subjects from the two given disciplines
+ * @param {string[]} discs the two disciplines
+ * @return {string[]} two random subjects
+ */
+ function randomSubjects(discs) {
+
+    return ["differentialligninger", "animationer"];
+}
+/** nr 7
  * Make string that informs user of disciplines and subjects
  * @param {string[]} discs The two disciplines
  * @param {string[]} subs The two subjects
@@ -50,8 +56,8 @@ function loadPrompt() {
 }
 
 function submitPressed() {
-    var description = $("#answer").val();
-    var resp = {
+    /*var description = $("#answer").val();
+     var resp = {
         "emne1": subs[0],
         "emne2": subs[1],
         "fag1": discs[0],
@@ -61,7 +67,7 @@ function submitPressed() {
     fetch("https://matquiz.dk/srpAi", {
         method: "POST",
         body: JSON.stringify(resp),
-    })
+    }) */
     loadPrompt();
 }
 
